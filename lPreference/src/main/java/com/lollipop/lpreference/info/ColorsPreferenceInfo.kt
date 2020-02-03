0eb1ed3f97521b81412e0b7327812d04397a65d5
lollipop <liujian594@hotmail.com>
2020-02-03 20:54:28 +0800

    基础工具的构建

---
+++ lPreference/src/main/java/com/lollipop/lpreference/info/ColorsPreferenceInfo.kt
@@ -0,0 +1,8 @@
+package com.lollipop.lpreference.info
+
+/**
+ * @author lollipop
+ * @date 2020-01-18 20:38
+ * 颜色偏好设置
+ */
+class ColorsPreferenceInfo(key: String): BasePreferenceInfo<IntArray>(key, IntArray(0))
