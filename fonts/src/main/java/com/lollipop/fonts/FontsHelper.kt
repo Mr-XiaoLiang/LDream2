6cf6818b0f1608d6e08a324335014c7e8275c4e1
lollipop <liujian594@hotmail.com>
2023-06-27 23:01:22 +0800

    feat: 新增字体模块，单独管理字体

---
+++ fonts/src/main/java/com/lollipop/fonts/FontsHelper.kt
@@ -0,0 +1,58 @@
+package com.lollipop.fonts
+
+import android.content.Context
+import android.graphics.Typeface
+
+object FontsHelper {
+
+    private val typefaceMap = HashMap<String, Typeface>()
+
+    private val fontList = ArrayList<LFont>()
+
+    private val fontCache = HashMap<String, LFont>()
+
+    fun load(context: Context, font: LFont): Typeface? {
+        return load(context, font.assetsName)
+    }
+
+    fun valueOf(assetsName: String): LFont {
+        val lFont = fontCache[assetsName]
+        if (lFont != null) {
+            return lFont
+        }
+        val newFont = LFont.valueOf(assetsName)
+        fontCache[assetsName] = newFont
+        return newFont
+    }
+
+    private fun load(context: Context, font: String): Typeface? {
+        val typeface = typefaceMap[font]
+        if (typeface != null) {
+            return typeface
+        }
+        try {
+            val newTypeface = Typeface.createFromAsset(context.assets, font)
+            typefaceMap[font] = newTypeface
+            return newTypeface
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    fun findListByAssets(context: Context): List<LFont> {
+        if (fontList.isNotEmpty()) {
+            return ArrayList(fontList)
+        }
+        val assets = context.assets
+        val list = assets.list("fonts") ?: return emptyList()
+        val lFonts = list.map { LFont(displayName = it, assetsName = "font/$it") }
+        fontList.clear()
+        fontList.addAll(lFonts)
+        lFonts.forEach {
+            fontCache[it.assetsName] = it
+        }
+        return lFonts
+    }
+
+}
---
+++ fonts/src/main/java/com/lollipop/fonts/LFont.kt
@@ -0,0 +1,31 @@
+package com.lollipop.fonts
+
+import android.content.Context
+import android.graphics.Typeface
+import android.widget.TextView
+
+class LFont(
+    val displayName: String,
+    val assetsName: String
+) {
+
+    companion object {
+        fun valueOf(assetsName: String): LFont {
+            var name = assetsName
+            val lastIndexOf = assetsName.lastIndexOf("/")
+            if (lastIndexOf >= 0 && lastIndexOf < assetsName.length - 1) {
+                name = assetsName.substring(lastIndexOf + 1)
+            }
+            return LFont(displayName = name, assetsName = assetsName)
+        }
+    }
+
+    fun load(context: Context): Typeface? {
+        return FontsHelper.load(context, this)
+    }
+
+    fun bind(textView: TextView) {
+        textView.typeface = load(textView.context)
+    }
+
+}
